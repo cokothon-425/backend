567d9d845adc79b4af9c36b5e8ac0f024f0a5d97
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-09-28 21:00:22 +0900

    chore: import 오류 수정 및 불필요한 controller 제거

--- src/main/java/cokothon/backend/book/controller/BookController.java
+++
@@ -1,7 +0,0 @@
-package cokothon.backend.book.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BookController {
-}
--- src/main/java/cokothon/backend/record/repository/RecordRepository.java
+++ src/main/java/cokothon/backend/record/repository/RecordRepository.java
@@ -1,5 +1,6 @@
 package cokothon.backend.record.repository;
 
+import cokothon.backend.record.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
